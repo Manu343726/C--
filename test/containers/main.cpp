9008b4f3330c16bbb46a373be1520e30d2040ff6
ZaruWright <samuel_gse@hotmail.com>
2017-01-31 21:08:00 +0100

    Doxygen generator changed

--- test/containers/main.cpp
+++ test/containers/main.cpp
@@ -6,10 +6,6 @@ using namespace cmm::containers;
 
 int main(int argc, char **argv)
 {
-	// #ifdef _DEBUG
-	// 	std::cout << "Hola" << std::endl;
 	::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-    
-	// return 0;
 }
